d98877391dd2c03b46b313b5913e76ed6da55235
athifrz1212 <athifrahman2000@gmail.com>
2024-11-18 00:46:11 +0530

    Item card and nav-bar added

--- src/app/pages/menu-page/menu-page.component.ts
+++ src/app/pages/menu-page/menu-page.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-menu-page',
-  templateUrl: './menu-page.component.html',
-  styleUrls: ['./menu-page.component.scss']
+  selector: "app-menu-page",
+  templateUrl: "./menu-page.component.html",
+  styleUrls: ["./menu-page.component.scss"],
 })
 export class MenuPageComponent {
-
+  itemList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 }
